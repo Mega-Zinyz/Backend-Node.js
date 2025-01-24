58e8b42b80916b7aea4f5ee061ce70dd45480b9a
Mega-Zinyz <megazinyz@gmail.com>
2025-01-24 23:35:39 +0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -36,7 +36,7 @@ router.post('/login', async (req, res) => {
                 user: {
                     no_user: user.no_user,
                     username: user.username,
-                    profil_url: `${process.env.RAILWAY_PUBLIC_DOMAIN}/profil_img/${user.profil_url}`,
+                    profil_url: `${process.env.RAILWAY_PUBLIC_DOMAIN}/assets/profil_img/${user.profil_url}`,
                 },
             });
         } else {
