0ed8d41ca863d875ab02f1c77e40bae6a40ac9ed
Mega-Zinyz <megazinyz@gmail.com>
2025-01-25 00:04:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.use((err, req, res, next) => {
 // Start the server
 app.listen(PORT, () => {
     const pid = process.pid;
-    const serverUrl = process.env.RAILWAY_STATIC_URL || `http://localhost:${PORT}`;
+    const serverUrl = process.env.RAILWAY_STATIC_URL;
     console.log(`Server is running on ${serverUrl} with PID: ${pid}`);
     logger.info(`Server started on ${serverUrl} with PID: ${pid}`);
     logger.info(`Environment: ${environment}`);
