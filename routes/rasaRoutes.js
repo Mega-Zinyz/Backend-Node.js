584da65d8d030f9be763f58e7c2bf99be4002aa1
Mega-Zinyz <megazinyz@gmail.com>
2025-01-30 05:38:48 +0700

    env fix

--- routes/rasaRoutes.js
+++ routes/rasaRoutes.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const { startRasa, stopRasa, restartRasa, getRasaProcess } = require('../rasa_scripts/rasaController');
 const path = require('path');
